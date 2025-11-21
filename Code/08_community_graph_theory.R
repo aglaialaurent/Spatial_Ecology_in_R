9f275d069e29c1db08c5fef75455ac8b9c274edc
aglaialaurent <aglaialaurent@gmail.com>
2025-11-21 09:56:21 +0100

    08_community_graph_theory.R

--- Code/08_community_graph_theory.R
+++ Code/08_community_graph_theory.R
@@ -7,10 +7,12 @@ species <- c("Algae", "Zooplankton", "Small Fish", "Large Fish", "Bird")
 #build 1 column
 predator <- c("Zooplankton", "Small Fish", "Large Fish", "Bird", "Bird")
 #another colum
-prey<- c("Algae", "Zooplankton", "Small Fish", "Small fish", "Large Fish")
+prey<- c("Algae", "Zooplankton", "Small Fish", "Small Fish", "Large Fish")
 
 #Build a data frame
 interactions <- data.frame(predator,prey)
 
 #Create graph, directed means that there is a direction from predator to prey
 g <- graph_from_data_frame(interactions, vertices = species, directed = TRUE)
+plot(g)
+
